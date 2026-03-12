5b796d3ab011283e7e0e7f0819484c69fcd7f4d4
AndersonDesign1 <josanderson25@gmail.com>
2026-03-12 15:58:14 +0100

    Refine marketing pages and site structure

---
+++ src/lib/utils/site.ts
@@ -0,0 +1,4 @@
+export const SITE_REPO_URL =
+  "https://github.com/AndersonDesign1/img-compression";
+export const SITE_ISSUES_URL = `${SITE_REPO_URL}/issues`;
+export const SITE_LICENSE_URL = `${SITE_REPO_URL}/blob/main/LICENSE`;
